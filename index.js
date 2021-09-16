aff5f51275fe4c9bba4eab63aaade26bbbc48df1
saurabh6677 <saurabhinpurani@gmail.com>
2021-09-16 06:55:14 +0530

    post request added

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
+const { Console } = require("console");
 const http = require("http");
-const queryString = require("querystring");
+const query = require("querystring");
 const server = http.createServer((request,response) => {
 
     let allData = "";
     request.on("data",(chunks) => {
-        allData += data;
+        allData += chunks.toString();
     });
 
     request.on("end", () => {
-        const final = queryString.parse(allData);
+        let final = query.parse(allData);
+        if(final.username === "saurabh" && final.pass === "raju")
+        {
+            //response.write("success !");
+            response.writeHead(200,{"Content-Type": "application/json",
+            });
+            let massage = JSON.stringify({massage : "login Success !"});
+            response.write(massage);
+        }
+        else{
+            response.writeHead(401,{"Content-Type" : "application/json"})
+            let massage = JSON.stringify({massage : "login Faild !"});
+            
+            response.write(massage);
+        }
         response.end();
     });
     

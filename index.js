684283bd72c346cd78c5b7298d65774499119788
saurabh6677 <saurabhinpurani@gmail.com>
2021-09-16 11:27:14 +0530

    first commit

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const http = require('http');
 const query = require('querystring');
 const server =  http.createServer((request,response) => {
     
-    const requestType = request.method;  /* it will return 'GET' or 'POST' */
+    
 
-    if(requestType === 'GET')
+    if(request.method === 'GET')
     {
         /* Write code for GET request */
         const userData  =  query.parse(request.url.replace('/?',''));
@@ -21,15 +21,45 @@ const server =  http.createServer((request,response) => {
             const massage = JSON.stringify({"massage" : "user unauthorised !"});
             response.write(massage);
         }
+
         response.end();
+        
     }
-    
+
     else{
-        /* write code for if request is post */
 
+        /* write code for (if request is post) post */
+        let userData = "";
+        request.on('data', (chunks) => {
+            userData += chunks.toString();
+        });
+
+        request.on('end', () => {
+           userData = query.parse(userData);
+           const username = userData.username;
+           const password = userData.password;
+           
+           if(username === "saurabh" && password == 1234)
+           {
+                response.writeHead(200,{'Content-Type' : 'application/json'});
+                const massage = JSON.stringify({"massage" : "use authorized !"});
+                response.write(massage);
+                
+           }
+           else
+           {
+                response.writeHead(401,{'Content-Type' : 'application/json'});
+                const massage = JSON.stringify({'massage' : 'user unautorized !'});
+                response.write(massage);
+                
+           }
+        });
+
+       
+        
     }
 
-    
+   
 });
 
 server.listen(8080);
